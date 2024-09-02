e5a626e89d6ec2932669e8cba291021784a2fac5
unknown <ananyekachhap25@gmail.com>
2024-09-02 21:56:18 +0530

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import './globals.css'
+import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,7 +20,9 @@ export default function RootLayout({
       <body className={inter.className}>
       <Navbar/>
       {children}
+      <Footer/>
       </body>
+      
       
     </html>
   );
