9290df46b7546483231fecad85305f7598d547e2
Manideep <alukamanideepreddy@gmail.com>
2024-09-28 17:36:28 +0530

    updated

--- src/components/Incentives.tsx
+++ src/components/Incentives.tsx
@@ -3,7 +3,7 @@ import Certificate from '../assets/cert.png'
 import Goodies from '../assets/good.png'
 import Invitation from '../assets/invite.png' 
 import Networking from '../assets/network4.png'
-import 'animate.css'
+
 
 const Incentives = () => {
   return (
@@ -15,33 +15,33 @@ const Incentives = () => {
 
                     <div className='grid justify-start grid-flow-col mb-4 x3:scale-80 x2:scale-75 x1:scale-90 xl:scale-100 xm:grid-flow-row'>
                         <div className='mr-1 grid justify-center xn:hover:scale-105 duration-300'>
-                            <img src={Certificate.src} alt="none" className='h-[120px] mr-3 grid items-center duration-500 xn:hover:drop-shadow-[0_0_15px_rgba(256,0,0,0.85)] animate__animated animate__zoomIn animate__delay-1s'/>
+                            <img src={Certificate.src} alt="none" className='h-[120px] mr-3 grid items-center duration-500 xn:hover:drop-shadow-[0_0_15px_rgba(256,0,0,0.85)] animate-zoom-in'/>
                         </div>
-                        <div className='ml-1 font-mono text-nf grid items-center h-[120px] opacity-85 animate__animated animate__slideInRight animate__fast'>Certificate of Acknowledgment, duly signed by the Chairman of Shaurya IIT Kharagpur and the President of Gymkhana IIT Kharagpur, upon the triumphant completion of the service period.</div>
+                        <div className='ml-1 font-mono text-nf grid items-center h-[120px] opacity-85 animate-slide-in-right'>Certificate of Acknowledgment, duly signed by the Chairman of Shaurya IIT Kharagpur and the President of Gymkhana IIT Kharagpur, upon the triumphant completion of the service period.</div>
                     </div>
 
 
                     <div className='grid justify-start grid-flow-col mb-4 x3:scale-80 x2:scale-75 x1:scale-90 xl:scale-100 xm:grid-flow-row'>
                         <div className='mr-1 grid justify-center  xn:hover:scale-105 duration-300'>
-                            <img src={Invitation.src} alt="none" className='h-[120px] mr-3 grid items-center duration-500 xn:hover:drop-shadow-[0_0_15px_rgba(256,0,0,0.85)] animate__animated animate__zoomIn animate__delay-1s'/>
+                            <img src={Invitation.src} alt="none" className='h-[120px] mr-3 grid items-center duration-500 xn:hover:drop-shadow-[0_0_15px_rgba(256,0,0,0.85)] animate-zoom-in'/>
                         </div>
-                        <div className='ml-1 font-mono text-nf grid items-center h-[120px] opacity-85 animate__animated animate__slideInLeft animate__fast'>Privilege to attend all formal events and programs organized by Shaurya IIT Kharagpur.</div>
+                        <div className='ml-1 font-mono text-nf grid items-center h-[120px] opacity-85 animate-slide-in-left'>Privilege to attend all formal events and programs organized by Shaurya IIT Kharagpur.</div>
                     </div>
 
 
                     <div className='grid justify-start grid-flow-col mb-4 x3:scale-80 x2:scale-75 x1:scale-90 xl:scale-100 xm:grid-flow-row'>
                         <div className='mr-1 grid justify-center xn:hover:scale-105 duration-300'>
-                            <img src={Goodies.src} alt="none" className='h-[120px] mr-3 grid items-center duration-500 xn:hover:drop-shadow-[0_0_15px_rgba(256,0,0,0.85)] animate__animated animate__zoomIn animate__delay-1s'/>
+                            <img src={Goodies.src} alt="none" className='h-[120px] mr-3 grid items-center duration-500 xn:hover:drop-shadow-[0_0_15px_rgba(256,0,0,0.85)] animate-zoom-in'/>
                         </div>
-                        <div className='ml-1 font-mono text-nf grid items-center h-[120px] opacity-85 animate__animated animate__slideInRight animate__fast'>Premium gift assortments and curated hampers presented upon the fest's conclusion.</div>
+                        <div className='ml-1 font-mono text-nf grid items-center h-[120px] opacity-85 animate-slide-in-right'>Premium gift assortments and curated hampers presented upon the fest's conclusion.</div>
                     </div>
                     
 
                     <div className='grid justify-start grid-flow-col mb-4 x3:scale-80 x2:scale-75 x1:scale-90 xl:scale-100 xm:grid-flow-row'>
                         <div className='mr-1 grid justify-center xn:hover:scale-105 duration-300'>
-                           <img src={Networking.src} alt="none" className='h-[120px] mr-3 grid items-center duration-500 xn:hover:drop-shadow-[0_0_15px_rgba(256,0,0,0.85)] animate__animated animate__zoomIn animate__delay-1s'/> 
+                           <img src={Networking.src} alt="none" className='h-[120px] mr-3 grid items-center duration-500 xn:hover:drop-shadow-[0_0_15px_rgba(256,0,0,0.85)] animate-zoom-in'/> 
                         </div>
-                        <div className='ml-1 font-mono text-nf grid items-center h-[120px] opacity-85 animate__animated animate__slideInLeft animate__fast'>Comprehensive networking with IIT Kharagpur students and representatives from a variety of other academic institutions.</div>
+                        <div className='ml-1 font-mono text-nf grid items-center h-[120px] opacity-85 animate-slide-in-left'>Comprehensive networking with IIT Kharagpur students and representatives from a variety of other academic institutions.</div>
                     </div>
 
                     
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,13 +44,23 @@ const config: Config = {
           '0%': { transform: 'translateX(100%)', opacity: '0' },
           '100%': { transform: 'translateX(0)', opacity: '1' },
         },
+        'slide-in-left': {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },},
+          
+          'zoom-in': {
+          '0%': { transform: 'scale(0)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        },
+
       },
       animation: {
         'gradient-move': 'gradient 5s ease infinite',
         'slide-up': 'slideUp 1s ease-out',
         'slide-down': 'slideDown 1s ease-out',
         'slide-in-right': 'slideInRight 1s ease-out',
-      
+        'slide-in-left': 'slide-in-left 1s ease-out', 
+        'zoom-in': 'zoom-in 1s ease-out',
       },
       backgroundSize: {
         'size-200': '200% 200%',
