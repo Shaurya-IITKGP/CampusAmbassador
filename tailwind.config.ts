e41dbe618a473373217bf67b507598e4f5c21c3a
Sahil-Sinha-11 <sahilsinha6969@gmail.com>
2024-09-30 21:55:50 +0530

    edited Incentive page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,40 +12,7 @@ const config: Config = {
         'lightOrange':"#F3B333",
         'darkOrange':'#B46100'
       },
-      keyframes: {
-        gradient: {
-          '0%, 100%': { 'background-position': '0% 50%' },
-          '50%': { 'background-position': '100% 50%' },
-        },
-        
-        slideUp: {
-          '0%': { transform: 'translateY(100%)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
-        },
-        slideDown: {
-          '0%': { transform: 'translateY(-100%)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
-        },
-        slideInRight: {
-          '0%': { transform: 'translateX(100%)', opacity: '0' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
-        },
-      },
-      animation: {
-        'gradient-move': 'gradient 5s ease infinite',
-        'slide-up': 'slideUp 1s ease-out',
-        'slide-down': 'slideDown 1s ease-out',
-        'slide-in-right': 'slideInRight 1s ease-out',
       
-      },
-      backgroundSize: {
-        'size-200': '200% 200%',
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   
   },
