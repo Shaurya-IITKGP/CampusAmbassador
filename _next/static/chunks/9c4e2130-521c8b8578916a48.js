27dd1ed1dcaf3b75f164f3f10da97b50987de4fc
Sahil-Sinha-11 <sahilsinha6969@gmail.com>
2024-10-04 00:16:08 +0530

    Deploy static site to GitHub Pages

---
+++ _next/static/chunks/9c4e2130-521c8b8578916a48.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[706],{5097:function(t,n,e){e.d(n,{MB4:function(){return i}});var r=e(1810);function i(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"M96 256h320M96 176h320M96 336h320"},child:[]}]})(t)}}}]);
